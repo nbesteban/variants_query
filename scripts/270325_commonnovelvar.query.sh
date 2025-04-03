67b6cb37207ab88e3ac42807f140af92dfb45149
Noriel Esteban <112158540+nbesteban@users.noreply.github.com>
2025-04-03 17:45:43 +0800

    Added search for benign variants

--- scripts/270325_commonnovelvar.query.sh
+++ scripts/270325_commonnovelvar.query.sh
@@ -25,40 +25,54 @@ echo "===Step 3: Annotating using SnpEff==="
 #Annotate the common novel variants (MAF>0.01) using SnpEff
 java -Xmx20g -jar ~/Softwares/snpEff/snpEff.jar -v hg38 ${out}novel_variants.common.vcf.gz -stats ${out}novel_variants.common.snpEff > ${out}novel_variants.common.snpEff.vcf
 bgzip -c ${out}novel_variants.common.snpEff.vcf > ${out}novel_variants.common.snpEff.vcf.gz
-#rm ${out}novel_variants.common.snpEff.vcf
+rm ${out}novel_variants.common.snpEff.vcf
 
 echo "===Step 4: Annotating using SIFT==="
 #Annotate using SIFT, single transcipt only
 mkdir ${out}novel_variants.common.sift
 java -jar ~/Softwares/SIFT4G_Annotator.jar -c -i ${out}novel_variants.common.vcf -d ../resources/GRCh38.83.chr/ -r ${out}novel_variants.common.sift
+bgzip -c ${out}novel_variants.common.sift/novel_variants.common_SIFTpredictions.vcf > ${out}novel_variants.common.sift/novel_variants.common_SIFTpredictions.vcf.gz
+rm ${out}novel_variants.common.sift/novel_variants.common_SIFTpredictions.vcf
 
 sed -n '1p;/DELETERIOUS/p' ${out}novel_variants.common.sift/novel_variants.common_SIFTannotations.xls | grep -v "*WARNING!" > ${out}novel_variants.common.sift_deleterious.txt
 sed -n '1p;/novel/p' ${out}novel_variants.common.sift_deleterious.txt > ${out}novel_variants.common.sift_deleterious_novel.txt
 sed -n '1p;/novel/!p' ${out}novel_variants.common.sift_deleterious.txt > ${out}novel_variants.common.sift_deleterious_dbSNP.txt
 
+var_count_del=$(cat ${out}novel_variants.common.sift_deleterious.txt | wc -l)
+var_count_del_novel=$(cat ${out}novel_variants.common.sift_deleterious_novel.txt | wc -l)
+var_count_del_dbSNP=$(cat ${out}novel_variants.common.sift_deleterious_dbSNP.txt | wc -l)
 count_del=$(awk -F '\t' '{print $6}' ${out}novel_variants.common.sift_deleterious.txt | sort -u | wc -l)
 count_del_novel=$(awk -F '\t' '{print $6}' ${out}novel_variants.common.sift_deleterious_novel.txt | sort -u | wc -l)
 count_del_dbSNP=$(awk -F '\t' '{print $6}' ${out}novel_variants.common.sift_deleterious_dbSNP.txt | sort -u | wc -l)
 
 #need to subtract 1 to remove the header line count since we counted it including the header line
-echo "The input callset has $(expr $count_del - 1) deleterious variant, of which $(expr $count_del_novel - 1) are found to be novel to the dataset and $(expr $count_del_dbSNP - 1) are annotated in dbSNP database." > ${out}/novel_variants_summary.txt
+echo "The input callset has $(expr $var_count_del - 1) deleterious variants that transcribed to $(expr $count_del - 1) genes. These deleterious variants consists of $(expr $var_count_del_novel - 1) novel variants to the dataset and $(expr $var_count_del_dbSNP - 1) already annotated in dbSNP database, which transcribed to $(expr $count_del_novel - 1) and $(expr $count_del_dbSNP - 1) genes, respectively." > ${out}/novel_variants_summary.txt
 
 echo "===Step 5: Annotating based on ClinVar Database==="
 #Annotate using SNPSift with reference to CLINVAR Database
 java -Xmx20g -jar ~/Softwares/snpEff/SnpSift.jar annotate ~/Softwares/snpEff/data/clinvar/clinvar_20250321.vcf.gz ${out}novel_variants.common.vcf > ${out}novel_variants.common.clinvar.vcf
 bgzip -c ${out}novel_variants.common.clinvar.vcf > ${out}novel_variants.common.clinvar.vcf.gz
-#rm ${out}novel_variants.common.clinvar.vcf
+rm ${out}novel_variants.common.clinvar.vcf
 
-bcftools view -i 'INFO/CLNSIG="Pathogenic"' ${out}novel_variants.common.clinvar.vcf.gz -Oz | bcftools query -f '[%CHROM %POS %TYPE %ID %GT %GQ %INFO/CLNDN %INFO/CLNSIG %INFO/CLNSIGCONF %INFO/CLNSIGINCL %INFO/CLNVC %INFO/GENEINFO\n]' > ${out}novel_variants.common.clinvar_patho.txt
+bcftools view -i 'INFO/CLNSIG="Pathogenic"' ${out}novel_variants.common.clinvar.vcf.gz -Oz | bcftools query -f '[%CHROM %POS %TYPE %ID %GT %GQ %INFO/CLNDN %INFO/CLNSIG %INFO/CLNSIGCONF %INFO/CLNSIGINCL %INFO/CLNVC %INFO/GENEINFO %AF\n]' > ${out}novel_variants.common.clinvar_patho.txt
 
-bcftools view -i 'INFO/CLNSIG="Likely_pathogenic"' ${out}novel_variants.common.clinvar.vcf.gz -Oz | bcftools query -f '[%CHROM %POS %TYPE %ID %GT %GQ %INFO/CLNDN %INFO/CLNSIG %INFO/CLNSIGCONF %INFO/CLNSIGINCL %INFO/CLNVC %INFO/GENEINFO\n]' > ${out}novel_variants.common.clinvar_likelypatho.txt
+bcftools view -i 'INFO/CLNSIG="Likely_pathogenic"' ${out}novel_variants.common.clinvar.vcf.gz -Oz | bcftools query -f '[%CHROM %POS %TYPE %ID %GT %GQ %INFO/CLNDN %INFO/CLNSIG %INFO/CLNSIGCONF %INFO/CLNSIGINCL %INFO/CLNVC %INFO/GENEINFO %AF\n]' > ${out}novel_variants.common.clinvar_likelypatho.txt
 
-bcftools view -i 'INFO/ONC="Oncogenic"' ${out}novel_variants.common.clinvar.vcf.gz -Oz | bcftools query -f '[%CHROM %POS %TYPE %ID %GT %GQ %INFO/CLNDN %INFO/CLNSIG %INFO/CLNSIGCONF %INFO/CLNSIGINCL %INFO/CLNVC %INFO/GENEINFO\n]' > ${out}novel_variants.common.clinvar_oncogenic.txt
+bcftools view -i 'INFO/CLNSIG="Benign"' ${out}novel_variants.common.clinvar.vcf.gz -Oz | bcftools query -f '[%CHROM %POS %TYPE %ID %GT %GQ %INFO/CLNDN %INFO/CLNSIG %INFO/CLNSIGCONF %INFO/CLNSIGINCL %INFO/CLNVC %INFO/GENEINFO %AF\n]' > ${out}novel_variants.common.clinvar_benign.txt
 
-count_patho=$(awk -F ' ' '{print $7}' ${out}novel_variants.common.clinvar_patho.txt | sort -u | wc -l)
-count_likelypatho=$(awk -F ' ' '{print $7}' ${out}novel_variants.common.clinvar_likelypatho.txt | sort -u | wc -l)
-count_oncogenic=$(awk -F ' ' '{print $7}' ${out}novel_variants.common.clinvar_oncogenic.txt | sort -u | wc -l)
+bcftools view -i 'INFO/CLNSIG="Likely_benign"' ${out}novel_variants.common.clinvar.vcf.gz -Oz | bcftools query -f '[%CHROM %POS %TYPE %ID %GT %GQ %INFO/CLNDN %INFO/CLNSIG %INFO/CLNSIGCONF %INFO/CLNSIGINCL %INFO/CLNVC %INFO/GENEINFO %AF\n]' > ${out}novel_variants.common.clinvar_likelybenign.txt
 
-echo "The input callset has $count_patho pathogenic variants, $count_likelypatho likely pathogenic variants, and $oncogenic oncogenic variants based on ClinVar database." >> ${out}novel_variants_summary.txt
+bcftools view -i 'INFO/ONC="Oncogenic"' ${out}novel_variants.common.clinvar.vcf.gz -Oz | bcftools query -f '[%CHROM %POS %TYPE %ID %GT %GQ %INFO/CLNDN %INFO/CLNSIG %INFO/CLNSIGCONF %INFO/CLNSIGINCL %INFO/CLNVC %INFO/GENEINFO %AF\n]' > ${out}novel_variants.common.clinvar_oncogenic.txt
+
+
+
+count_patho=$(awk -F ' ' '{print $1}' ${out}novel_variants.common.clinvar_patho.txt | sort -u | wc -l)
+count_likelypatho=$(awk -F ' ' '{print $1}' ${out}novel_variants.common.clinvar_likelypatho.txt | sort -u | wc -l)
+count_benign=$(awk -F ' ' '{print $1}' ${out}novel_variants.common.clinvar_benign.txt | sort -u | wc -l)
+count_likelybenign=$(awk -F ' ' '{print $1}' ${out}novel_variants.common.clinvar_likelybenign.txt | sort -u | wc -l)
+count_oncogenic=$(awk -F ' ' '{print $1}' ${out}novel_variants.common.clinvar_oncogenic.txt | sort -u | wc -l)
+
+
+echo "The input callset has $count_patho pathogenic variants, $count_likelypatho likely pathogenic variants, $count_benign benign variants, $count_likelybenign likely benign variants, and $oncogenic oncogenic variants based on ClinVar database." >> ${out}novel_variants_summary.txt
 
 echo "Completed"
